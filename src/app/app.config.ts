75c6e33690ae9febf0c67b8091e74257f7d08eb4
jonathanszekely3@gmail.com <jonathanszekely3@gmail.com>
2025-08-21 18:50:57 +0300

    Emnvironment setup done

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,28 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Nora from '@primeng/themes/nora';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+        theme: {
+            preset: Nora,
+            options: {
+              cssLayer: {
+                  name: 'primeng',
+                  order: 'tailwind-base, primeng, tailwind-utilities'
+              }
+          }
+        }
+    })
+  ],
 };
